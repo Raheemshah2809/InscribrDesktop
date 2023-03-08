011bac0fa472850f9e4eb0c31e857d3efd562f45
Raheem Shah <Raheemshah2809@gmail.com>
2023-03-08 14:44:24 +0000

    0.39

---
+++ main.js
@@ -0,0 +1,20 @@
+const {
+    app,
+    BrowserWindow
+} = require('electron')
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    win.loadURL('https://inscribr.org')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+})
